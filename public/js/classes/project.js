676fad57abff2dc4f2cb3e9af1026797303d4725
Alison-Doyle <adoyle@sleaterandco.com>
2023-10-18 17:01:17 +0100

    :sparkles: Added chart to card

---
+++ public/js/classes/project.js
@@ -0,0 +1,10 @@
+export class Project {
+    // Constructors
+    constructor(name, company, dueBy, inprogress, completed) {
+        this.Name = name;
+        this.Company = company;
+        this.DueByDate = dueBy;
+        this.Inprogress = inprogress;
+        this.Completed = completed;
+    }
+}
