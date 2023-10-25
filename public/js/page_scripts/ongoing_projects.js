16f507b0f0e3cee35e2644bfdff57547a5c7d4da
Alison-Doyle <adoyle@sleaterandco.com>
2023-10-25 17:01:57 +0100

    Working on adding chart

--- public/js/page_scripts/ongoing_projects.js
+++ public/js/page_scripts/ongoing_projects.js
@@ -1,3 +1,7 @@
+"use strict"
+
+import "https://cdn.jsdelivr.net/npm/chart.js";
+
 (function () {
     // Populating page w/ projects
     PopulateDisplayWithProjects();
@@ -16,6 +20,9 @@ function PopulateDisplayWithProjects() {
     let projectsInformation = JSON.parse(localStorage.getItem(ProjectLocalStorageName));
 
     for (let i = 0; i < projectsInformation.length; i++) {
+        // Creating IDs for charts
+        let canvasId = projectsInformation[i].ProjectName.replace(" ", "-") + "-Canvas";
+
         // Formating project card
         let projectCard = `
         <div class="col-12 col-md-6 col-lg-6 col-xl-3">
@@ -24,10 +31,15 @@ function PopulateDisplayWithProjects() {
                     <span class="project-number text-body-secondary">(${projectsInformation[i].ProjectNumber})</span>
                 </h2>
                 <h3 class="card-subtitle mb-2 text-body-secondary">${projectsInformation[i].CompanyName}</h3>
+                <div>
+                    <canvas id="${canvasId}"></canvas>
+                </div>
             </div>
         </div>`;
 
         cardContainer.innerHTML += projectCard;
+
+        CreateChart(canvasId);
     }
 }
 
@@ -39,4 +51,58 @@ function RedirectToProjectCreation() {
 
     // Adding location attribute to redirect
     createNewProjectButton.href = NewProjectPage;
+}
+
+function CreateChart(chartId) {
+    // Html elements
+    let chart = document.getElementById(chartId);
+
+    
+    const projectPercentage = "56%"
+
+    // Chart dummy data
+    let dummyData = {
+        labels: [
+            "Not Started/Missing Documents",
+            "In Progress/Documents Waiting Approval",
+            "Completed"
+        ],
+        datasets: [{
+            data: [3, 5, 1],
+            borderWidth: 1,
+            backgroundColor: ["#FF5733", "#FFC300", "#9FE2BF"],
+            cutout: "76%"
+        }]
+    };
+
+    const projectCompletionPercentage = {
+        id: 'percentage',
+        beforeDraw(chart, args, options) {
+            // Calculating the center point of doughnut chart (ignoring legend)
+            const { ctx, chartArea: { top, right, bottom, left, width, height } } = chart;
+            ctx.save();
+            let centerPointXAxisValue = width / 2;
+            let centerPointYAxisValue = top + (height / 2);
+
+            // Adding text to doughnut chart
+            ctx.font = '2rem sans-serif';
+            ctx.textAlign = "center";
+            ctx.textStyle = "black";
+            ctx.fillText(projectPercentage, centerPointXAxisValue, centerPointYAxisValue);
+        }
+    };
+
+    // Creating chart
+    new Chart(chart, {
+        type: 'doughnut',
+        data: dummyData,
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+        },
+        plugins: [
+            projectCompletionPercentage
+        ]
+    });
+
 }
